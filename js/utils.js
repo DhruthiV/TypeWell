631fdcbb277fb84d0d629e1025065728708289c0
DhruthiV <dhruthivenkateshheerguppe@gmail.com>
2026-05-20 22:22:15 +0530

    feat: add refresh icon and implement font size adjustment functionality

--- js/utils.js
+++ js/utils.js
@@ -11,3 +11,36 @@ function updateModeDisplay(mode) {
     progressFill.style.width = "0%";
   }
 }
+
+const MAX_FONTSIZE = 36;
+const MIN_FONTSIZE = 12;
+let currentSize = 16;
+const increaseFontBtn = document.getElementById("increase-font");
+const decreaseFontBtn = document.getElementById("decrease-font");
+const displayFontSize = document.querySelector("span#font-size-display");
+
+function updateFontSize(delta) {
+  let newSize = currentSize + delta;
+  if (newSize < MIN_FONTSIZE || newSize > MAX_FONTSIZE) return;
+
+  currentSize = newSize;
+  passageContent.style.fontSize = `${currentSize}px`;
+
+  updateButtonStates(); //To change colors
+}
+
+function updateButtonStates() {
+  displayFontSize.textContent = `${currentSize}px`;
+
+  increaseFontBtn.disabled = currentSize >= MAX_FONTSIZE;
+  decreaseFontBtn.disabled = currentSize <= MIN_FONTSIZE;
+}
+
+increaseFontBtn.addEventListener("click", () => {
+  updateFontSize(1);
+});
+decreaseFontBtn.addEventListener("click", () => {
+  updateFontSize(-1);
+});
+
+updateButtonStates();
